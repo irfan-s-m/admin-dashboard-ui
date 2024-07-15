1e4d2bbb7f7aa8d2bf3c663a2f69fd63d001ae54
irfan-s-m <know.cse@gmail.com>
2024-07-16 00:02:30 +0600

    Pie chart ongoing

--- src/components/chartBox/PieChartBox.jsx
+++ src/components/chartBox/PieChartBox.jsx
@@ -1,4 +1,4 @@
-import { Cell, Pie, PieChart } from "recharts";
+import { Cell, Pie, PieChart, ResponsiveContainer, Tooltip } from "recharts";
 import { pieChartData } from "../../utils/data";
 
 const PieChartBox = () => {
@@ -6,39 +6,41 @@ const PieChartBox = () => {
     const chartColor = pieChartData.map(data => data.color)
     const color = [...chartColor];
     return (
-        <div>
-            <PieChart width={800} height={400} onMouseEnter={this.onPieEnter}>
-                <Pie
-                    data={data}
-                    cx={120}
-                    cy={200}
-                    innerRadius={60}
-                    outerRadius={80}
-                    fill={color}
-                    paddingAngle={5}
-                    dataKey="value"
-                >
-                    {data.map((entry, index) => (
-                        <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-                    ))}
-                </Pie>
-                <Pie
-                    data={data}
-                    cx={420}
-                    cy={200}
-                    startAngle={180}
-                    endAngle={0}
-                    innerRadius={60}
-                    outerRadius={80}
-                    fill="#8884d8"
-                    paddingAngle={5}
-                    dataKey="value"
-                    >
-                    {data.map((entry, index) => (
-                        <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-                    ))}
-                </Pie>
-            </PieChart>
+        <div className="flex flex-col gap-2">
+            <div>
+                <h1 className="text-xl font-semibold">Leads by Source</h1>
+            </div>
+            <div>
+                <ResponsiveContainer width="100%" height="100%" aspect={0.75}>
+                    <PieChart>
+                        <Tooltip
+                        contentStyle={{background:'white', border:'none', borderRadius:'30px'}}></Tooltip>
+                        <Pie
+                            data={data}
+                            innerRadius={75}
+                            outerRadius={95}
+                            fill={color}
+                            paddingAngle={5}
+                            dataKey="value"
+                        >
+                            {data.map((entry) => (
+                                <Cell key={entry.name} fill={entry.color} />
+                            ))}
+                        </Pie>
+                    </PieChart>
+                </ResponsiveContainer>
+            </div>
+            <div className="grid grid-cols-2 mt-8">
+                {
+                    data.map((data, idx) => <div className="flex gap-4 items-center" key={idx}>
+                        <span>.</span>
+                        <div className="">
+                        <p>{data.name}</p>
+                        <p>{data.value}</p>
+                        </div>
+                    </div>)
+                }
+            </div>
         </div>
     );
 };
